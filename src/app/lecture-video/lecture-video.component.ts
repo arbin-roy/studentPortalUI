69559c87ba65cc3da0eaed44cef10b718644731f
arbin-roy <arbin-roy@users.noreply.github.com>
2021-05-11 22:39:08 +0530

    Initial change Added almost everything

---
+++ src/app/lecture-video/lecture-video.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lecture-video',
+  templateUrl: './lecture-video.component.html',
+  styleUrls: ['./lecture-video.component.scss']
+})
+export class LectureVideoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
