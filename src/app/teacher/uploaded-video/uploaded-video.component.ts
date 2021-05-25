62b254415591e984918ce53343378c5cd6107b65
arbin-roy <arbin-roy@users.noreply.github.com>
2021-05-25 23:40:50 +0530

    uploaded video module done

---
+++ src/app/teacher/uploaded-video/uploaded-video.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-uploaded-video',
+  templateUrl: './uploaded-video.component.html',
+  styleUrls: ['./uploaded-video.component.scss']
+})
+export class UploadedVideoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
